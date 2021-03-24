1b3dffc9b2dda36583270dde73d3c6dca2221827
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2021-03-24 20:59:21 +1100

    bash_sleep

---
+++ main.py
@@ -0,0 +1,23 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='bash_sleep',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+run_this = BashOperator(
+    task_id='sleep_60',
+    bash_command='sleep 60',
+    dag=dag,
+)
