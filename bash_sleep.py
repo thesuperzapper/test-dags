005c00e2a4847f5bf45c9b031e30cce2d3131be1
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2022-03-25 17:13:13 +1100

    support both airflow 1 & 2

--- bash_sleep.py
+++ bash_sleep.py
@@ -1,26 +1,31 @@
 from datetime import timedelta
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+
+# import with try/except to support both airflow 1 and 2
+try:
+    from airflow.operators.bash import BashOperator
+except ModuleNotFoundError:
+    from airflow.operators.bash_operator import BashOperator
 
 from test_package.functions import create_bash_task
 
 args = {
-    'owner': 'airflow',
+    "owner": "airflow",
 }
 
 dag = DAG(
-    dag_id='bash_sleep',
+    dag_id="bash_sleep",
     default_args=args,
-    schedule_interval='0 0 * * *',
+    schedule_interval="0 0 * * *",
     start_date=days_ago(2),
     dagrun_timeout=timedelta(minutes=60),
 )
 
 run_this = BashOperator(
-    task_id='sleep_60',
-    bash_command='sleep 60',
+    task_id="sleep_60",
+    bash_command="sleep 60",
     dag=dag,
 )
 
--- test_package/functions.py
+++ test_package/functions.py
@@ -1,9 +1,13 @@
-from airflow.operators.bash import BashOperator
+# import with try/except to support both airflow 1 and 2
+try:
+    from airflow.operators.bash import BashOperator
+except ModuleNotFoundError:
+    from airflow.operators.bash_operator import BashOperator
 
 
 def create_bash_task(dag):
     return BashOperator(
-        task_id='bash_task_from_function',
-        bash_command='sleep 60',
+        task_id="bash_task_from_function",
+        bash_command="sleep 60",
         dag=dag,
     )
