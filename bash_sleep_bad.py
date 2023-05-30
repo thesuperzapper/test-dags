dbf7d10391690a177fa5d53ed43b2d19ec83b345
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2023-05-29 17:48:51 -0700

    add bad dag

---
+++ bash_sleep_bad.py
@@ -0,0 +1,36 @@
+import datetime
+from datetime import timedelta
+
+import pendulum
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+# import with try/except to support both airflow 1 and 2
+try:
+    from airflow.operators.bash import BashOperator
+except ModuleNotFoundError:
+    from airflow.operators.bash_operator import BashOperator
+
+from test_package.functions import create_bash_task
+
+args = {
+    "owner": "airflow",
+}
+
+dag = DAG(
+    dag_id="bash_sleep_bad",
+    default_args=args,
+    schedule_interval="0 0 * * *",
+    start_date=days_ago(1),
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+run_this = BashOperator(
+    task_id="sleep_60",
+    bash_command="sleep 60",
+    ## bad start_date (after 9999-12-31)
+    start_date=datetime.datetime(9999, 12, 31).timestamp() + 24 * 60 * 60,
+    dag=dag,
+)
+
+run_that = create_bash_task(dag)
