dc604863f46085c9bd1cc2f448260c44057fb8c3
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2024-05-05 14:59:01 -0700

    dont cancel external job unless TaskInstance is NOT running or deferred

--- test_on_kill_deferred.py
+++ test_on_kill_deferred.py
@@ -8,10 +8,14 @@ import pendulum
 from airflow import DAG
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
+from airflow.models.taskinstance import TaskInstance
+from airflow.settings import Session
 from airflow.triggers.base import BaseTrigger, TriggerEvent
 from airflow.utils import timezone
 from airflow.utils.context import Context
 from airflow.utils.dates import days_ago
+from airflow.utils.session import provide_session
+from airflow.utils.state import TaskInstanceState
 from pendulum.datetime import datetime
 
 
@@ -22,12 +26,14 @@ class DateTimeTriggerWithCancel(BaseTrigger):
         self,
         dag_id: str,
         task_id: str,
+        run_id: str,
         statement_name: str,
         moment: datetime.datetime,
     ):
         super().__init__()
         self.dag_id = dag_id
         self.task_id = task_id
+        self.run_id = run_id
         self.statement_name = statement_name
 
         # set and validate the moment
@@ -46,10 +52,37 @@ class DateTimeTriggerWithCancel(BaseTrigger):
             {
                 "dag_id": self.dag_id,
                 "task_id": self.task_id,
+                "run_id": self.run_id,
                 "statement_name": self.statement_name,
                 "moment": self.moment,
             },
         )
+
+    @provide_session
+    def get_task_instance(self, session: Session) -> TaskInstance:
+        query = session.query(TaskInstance).filter(
+            TaskInstance.dag_id == self.dag_id,
+            TaskInstance.task_id == self.task_id,
+            TaskInstance.run_id == self.run_id,
+        )
+        task_instance = query.one_or_none()
+        if task_instance is None:
+            raise AirflowException(
+                f"TaskInstance {self.dag_id}.{self.task_id} with run_id {self.run_id} not found"
+            )
+        return task_instance
+
+    def safe_to_cancel(self) -> bool:
+        """
+        Whether it is safe to cancel the external job which is being executed by this trigger.
+        This is to avoid the case that `asyncio.CancelledError` is called because the trigger itself is stopped.
+        Because in those cases, we should NOT cancel the external job.
+        """
+        task_instance = self.get_task_instance()
+        return task_instance.state not in {
+            TaskInstanceState.RUNNING,
+            TaskInstanceState.DEFERRED,
+        }
 
     async def run(self) -> AsyncIterator[TriggerEvent]:
         self.log.info("trigger starting")
@@ -68,20 +101,22 @@ class DateTimeTriggerWithCancel(BaseTrigger):
                     "moment": self.moment,
                 }
             )
-            return
 
         except asyncio.CancelledError:
             self.log.info(f"asyncio.CancelledError was called")
             if self.statement_name:
-                # Cancel the query (mock by writing to a file)
-                output_folder = (
-                    f"/tmp/testing/on_kill_deferred/{self.dag_id}/{self.task_id}"
-                )
-                os.makedirs(output_folder, exist_ok=True)
-                with open(f"{output_folder}/log_trigger.txt", "a") as f:
-                    f.write(
-                        f"asyncio.CancelledError was called: {self.statement_name}\n"
+                if self.safe_to_cancel():
+                    # Cancel the query (mock by writing to a file)
+                    output_folder = (
+                        f"/tmp/testing/on_kill_deferred/{self.dag_id}/{self.task_id}"
                     )
+                    os.makedirs(output_folder, exist_ok=True)
+                    with open(f"{output_folder}/log_trigger.txt", "a") as f:
+                        f.write(
+                            f"asyncio.CancelledError was called: {self.statement_name}\n"
+                        )
+                else:
+                    self.log.warning("Triggerer probably stopped, not cancelling query")
             else:
                 self.log.info("self.statement_name is None")
         except Exception as e:
@@ -115,6 +150,7 @@ class TestDeferredOperator(BaseOperator):
             trigger=DateTimeTriggerWithCancel(
                 dag_id=self.dag.dag_id,
                 task_id=self.task_id,
+                run_id=context["run_id"],
                 statement_name=self.statement_name,
                 moment=self.moment,
             ),
